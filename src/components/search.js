1d84c6904d5a92c431d0307ee2a4cd5e69838ef6
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-03-20 12:42:30 +0000

    Implement handleChange() function to track searchValue

--- src/components/search.js
+++ src/components/search.js
@@ -3,9 +3,20 @@ import { useState } from "react";
 
 export function Search() {
   const [searchValue, setSearchValue] = useState("");
+
+  const handleChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
   return (
     <header>
-      <input type="text" placeholder="Type a name..." value={searchValue} />;
+      <input
+        type="text"
+        placeholder="Type a name..."
+        value={searchValue}
+        onChange={handleChange}
+      />
+      ;
     </header>
   );
 }
