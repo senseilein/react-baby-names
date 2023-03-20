d66ad71bcc99819fa99ede060905f96172f385e6
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-03-20 11:52:51 +0000

    Destructure props object

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 //egghead.io/lessons/react-pass-data-down-to-children-component-with-props-in-react
 
 function App({ names }) {
-  // no need to import name here because it is received in parent Component = at the root of the app (in index.js)
+  // no need to import name here because it is received from parent Component = at the root of the app (in index.js)
   return <pre>{JSON.stringify(names, null, 2)}</pre>;
 }
 
