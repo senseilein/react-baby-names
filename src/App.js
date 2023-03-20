659f8128f6a760d379d669defe3ea46786bd46f2
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-03-20 11:51:54 +0000

    Receive names array as a props in App Component

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+//egghead.io/lessons/react-pass-data-down-to-children-component-with-props-in-react
+
+function App({ names }) {
+  // no need to import name here because it is received in parent Component = at the root of the app (in index.js)
+  return <pre>{JSON.stringify(names, null, 2)}</pre>;
+}
+
+export default App;
