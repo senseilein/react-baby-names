d6b16f7e9419fe2a4755b0d4d5fee022c0a31065
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-03-20 13:14:53 +0000

    Focus input field with useRef and useEffect hooks

--- src/components/search.js
+++ src/components/search.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 export function Search() {
   const [searchValue, setSearchValue] = useState("");
@@ -8,9 +8,24 @@ export function Search() {
     setSearchValue(event.target.value);
   };
 
+  //we need the input element to be focused on load so that user can just start typing
+  // we need useRef to keep reference to the input element
+  const inputRef = useRef();
+  // then add a ref prop to our input and set it to inputRef
+
+  console.log(inputRef);
+  // current property is the input itself
+
+  // input element will have focus as soon as the Search Component is loaded
+  // dependency array = empty because we only want this function to run once
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   return (
     <header>
       <input
+        ref={inputRef}
         type="text"
         placeholder="Type a name..."
         value={searchValue}
