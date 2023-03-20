2ea09f9b21014ef3b2f3c13226d9b29736db46ed
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-03-20 14:07:17 +0000

    Create filteredNames to display the names that includes the searchValue

--- src/components/name-picker.js
+++ src/components/name-picker.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 export function NamePicker({ names, searchValue }) {
+  // in the <ul> below We want to display the names that includes the searchValue
+  const filteredNames = names.filter((entry) =>
+    entry.name.toLowerCase().includes(searchValue)
+  );
+  // it will initially renders the whole list
+
   return (
     <div id="NamePicker">
       <ul>
-        {names.map((entry) => {
+        {filteredNames.map((entry) => {
           return (
             <li className={entry.sex} key={entry.id}>
               <button>{entry.name}</button>
