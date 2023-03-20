69781f18a7e42350afa2d4b987de067f49eae2e8
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-03-20 11:57:57 +0000

    Render a button/list element for each name in the array

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import React from "react";
 
 function App({ names }) {
   // no need to import name here because it is received from parent Component = at the root of the app (in index.js)
-  return <pre>{JSON.stringify(names, null, 2)}</pre>;
+  // return (<pre>{JSON.stringify(names, null, 2)}</pre>)
+  return (
+    <ul>
+      {names.map((entry) => {
+        return (
+          <li className={entry.sex} key={entry.id}>
+            <button>{entry.name}</button>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default App;
